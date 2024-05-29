4bdeb9213f296b6f870fc8197efe96708d70d4dc
Afzal <168180044+afzall07@users.noreply.github.com>
2024-05-29 11:04:10 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3); // we are using nodeJS, not browser
+
+console.log(3 
+    +
+     3); // code readability should be high
+
+console.log("Afzal");
+
+// dataTypes
+
+let name = "Afzal"; // string
+let age = 18; //number
+let isLoggedIn = true; //boolean
+
+// Primitive data types
+
+// number => rang 2 to the power 53
+// bigint
+// string => ''/""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// Non-Primitive data types
+
+// object
+// null
+// undefined
+
+console.log(typeof null); // type object
+console.log(typeof undefined); // type undefined
