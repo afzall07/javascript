d2463182eca59435a490995780bde8ec97fb76da
Afzal <168180044+afzall07@users.noreply.github.com>
2024-05-29 09:53:20 +0000

    01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "afjalmalik@gmail.com";
+var accountPassword = "123";
+accountCity = "Sambhal";
+
+//accountId = 12  // not allowed 
+
+accountEmail = "riyazulmalik@gamil.com";
+accountPassword = "234";
+accountCity = "Bilari";
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
