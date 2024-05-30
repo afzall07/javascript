af3a37b19dcd078f77e7dd862a61118427092f6b
Afzal <168180044+afzall07@users.noreply.github.com>
2024-05-30 11:02:27 +0000

    datatypes summery

---
+++ 01_basics/05_dataTypes-summery.js
@@ -0,0 +1,37 @@
+// Primitive Data Types 
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const name = "Afal" // String
+const score = 100; // Number
+const isFollow = true; // boolean
+const temp = null; // null
+let userName; // undefined
+
+const Id = Symbol('123'); // Symbol
+const anotherId = Symbol('123'); 
+
+//  console.log(Id === anotherId);
+
+const bigNumber = 44354534566767756566n; //BigInt
+// console.log(typeof bigNumber);
+
+
+// Reference (Non-Primitive)
+
+// Arrays, Objects, Functions
+
+const cars = ["Audi", "BMW", "Thar"]; // array
+
+// object
+let myObj = {
+    myName: "afzal",
+    age: 18,
+    isFollow: true,
+}
+// console.log(myObj);
+
+// function
+const myFunction = function () {
+    console.log("Hello World");
+}
