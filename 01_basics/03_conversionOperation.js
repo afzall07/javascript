05ee51febc81c15b8a36b53e9ccdea371b831a50
Afzal <168180044+afzall07@users.noreply.github.com>
2024-05-30 06:22:17 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,4 +20,24 @@
   let someNumber = 33;
 
   let stringNumber = String(someNumber);
-  console.log( typeof stringNumber);
+  // console.log( typeof stringNumber);
+
+
+  // ********** operations **************
+
+  let value = 3;
+  let negValue = -value;
+  // console.log(negValue);
+
+  let str1 = "hello";
+  let str2 = " Afzal";
+
+  let str3 = str1 + str2;
+  // console.log(str3);
+
+  // console.log("1" + "2"); // => 12
+  // console.log(1 + "2"); // => 12
+  // console.log("1" + 2 + 2); // => 122
+  // console.log(1 + 2 + "2"); // => 32
+
+  // console.log( (3 + 4) * 5 % 3); 
