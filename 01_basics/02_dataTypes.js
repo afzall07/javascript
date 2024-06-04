a19a25585d90bbb0b821fb944224cc1737de7f26
Afzal <168180044+afzall07@users.noreply.github.com>
2024-06-04 04:55:30 +0000

    change in datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -14,22 +14,6 @@ let name = "Afzal"; // string
 let age = 18; //number
 let isLoggedIn = true; //boolean
 
-// Primitive data types
-
-// number => rang 2 to the power 53
-// bigint
-// string => ''/""
-// boolean => true/false
-// null => standalone value
-// undefined =>
-// symbol => unique
-
-
-// Non-Primitive data types
-
-// object
-// null
-// undefined
 
 console.log(typeof null); // type object
 console.log(typeof undefined); // type undefined
