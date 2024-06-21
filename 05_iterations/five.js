343dce0766de1e28ba4d17438ec59dfc8f6b8b26
Afzal <168180044+afzall07@users.noreply.github.com>
2024-06-21 18:36:28 +0000

    for each loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,38 @@
+const coding = ["js", "java", "python", "cpp", "ruby"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "Javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+} )
