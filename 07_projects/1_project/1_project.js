625c6797e06deb9b90aaab5883d4f0f57098c5ae
Afzal <168180044+afzall07@users.noreply.github.com>
2024-12-17 10:29:19 +0000

    project-1 bg changer

--- 07_projects/1_project/1_project.js
+++ 07_projects/1_project/1_project.js
@@ -7,20 +7,31 @@ buttons.forEach(function (button) {
     button.addEventListener('click', function (e) {
         // console.log(e);
         // console.log(e.target);
-        if (e.target.id === 'grey') {
-            body.style.backgroundColor = e.target.id
-        }
-        if (e.target.id === 'red') {
-            body.style.backgroundColor = e.target.id
-        }
-        if (e.target.id === 'blue') {
-            body.style.backgroundColor = e.target.id
-        }
-        if (e.target.id === 'green') {
-            body.style.backgroundColor = e.target.id
-        }
-        if (e.target.id === 'purple') {
-            body.style.backgroundColor = e.target.id
+        // if (e.target.id === 'grey') {
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if (e.target.id === 'red') {
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if (e.target.id === 'blue') {
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if (e.target.id === 'green') {
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if (e.target.id === 'purple') {
+        //     body.style.backgroundColor = e.target.id
+        // }
+
+        // use of switch case
+
+        switch (e.target.id) {
+            case e.target.id:
+                body.style.backgroundColor = e.target.id
+                break;
+
+            default:
+                break;
         }
 
 
