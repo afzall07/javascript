cb1f0dd53fbef3e1794e24bcbdc06f37d763f7d4
Afzal <168180044+afzall07@users.noreply.github.com>
2024-12-20 15:31:47 +0000

    project 4 number guessing game

---
+++ 07_projects/4_project/script.js
@@ -0,0 +1,32 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const low0rHi = document.querySelector('.low0rHi')
+const statOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+
+let prevGuess= [];
+let numOfGuess=1;
+
+let playGame= true;
+
+function validateGuess(guess){
+    //
+}
+
+function checkGuess(guess){
+    //
+}
+
+function displayGuess(guess){
+    //
+}
+
+function displayMessage(message) {
+    //
+}
+
