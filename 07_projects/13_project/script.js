3cd898bec3f9627d0b13c594fef82245aa0e7fa6
afzall07 <afjalmalik75000@gmail.com>
2025-08-22 11:52:41 +0530

    change in js file

--- 07_projects/13_project/script.js
+++ 07_projects/13_project/script.js
@@ -2,7 +2,7 @@ const input = document.getElementById('user-input')
 const checkBtn = document.getElementById('check-btn')
 const clearBtn = document.getElementById('clear-btn')
 const results = document.getElementById('results-div')
-
+// regex
 const phoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
 
 checkBtn.addEventListener("click", () => {
